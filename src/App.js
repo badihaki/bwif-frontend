710cfef8453a01e4f0c93c50c96ccc005178c183
Badi-Haki Martin <jojackblack@gmail.com>
2023-07-27 03:29:45 -0400

    set development to run on port 4000, client can connect/communicate with api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    fetch("/hello").then(r=>r.json()).then(data=>{
+      console.log(data);
+      setCount(data.count);
+    });
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Count: {count}
         </p>
         <a
           className="App-link"
